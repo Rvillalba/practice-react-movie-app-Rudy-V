731e6dfbff84a84c115cc83d44ef1b441f848fb3
eagleeye1689@gmail.com <eagleeye1689@gmail.com>
2025-07-02 15:13:12 -0500

    Working genre filter displays correct movies

--- movie-app/components/MovieSelector.jsx
+++ movie-app/components/MovieSelector.jsx
@@ -1,35 +1,39 @@
+import {useState} from "react";
 //Test Data
 const movies = [
-    {title: "Die Hard", genre: "action"},
-    {title: "Braveheart", genre: "action"},
-    {title: "Space Balls", genre: "comedy"},
-    {title: "50 First Dates", genre: "comedy"},
-    {title: "It Follows", genre: "horror"},
-    {title: "Saw", genre: "horror"}
+    {id: 1, title: "Die Hard", genre: "Action"},
+    {id: 2, title: "Braveheart", genre: "Action"},
+    {id: 3, title: "Space Balls", genre: "Comedy"},
+    {id:4, title: "50 First Dates", genre: "Comedy"},
+    {id: 5, title: "It Follows", genre: "Horror"},
+    {id: 6, title: "Saw", genre: "Horror"}
 ];
-const {title, genre} = movies
-const [selectedGenre, setSelectedGenre] = useState("")
 const DropDown = () => {
+const [selectedGenre, setSelectedGenre] = useState("");
+const handleChange = (event) => {
+    setSelectedGenre(event.target.value);
+};
+const filteredMovies = selectedGenre === '' ? movies : movies.filter(movie => movie.genre === selectedGenre)
      return (
-        <div className="dropdown"><form>
-                <select className="menu">
-                    <option>
-                        Select Movie Genre
-                    </option>
-                    <option className="menu-item">
-                        Action
-                    </option>
-                    <option className="menu-item">
-                        Comedy
-                    </option>
-                    <option className="menu-item">
-                        Horror
-                    </option>
+        <div className="dropdown">
+            <h1>Select Movie Genre Filter</h1>
+            <select onChange={handleChange} value={selectedGenre}>
+                    <option className="menu-item" value=""> All Genres</option>
+                    <option className="menu-item" value="Action" >Action</option>
+                    <option className="menu-item" value="Comedy" >Comedy</option>
+                    <option className="menu-item" value="Horror" >Horror</option>
                 </select>
             <button>Fetch Movie</button>
-            </form>
+            <h2>Movies:</h2>
+            <ul>
+                {filteredMovies.map(movie => (
+                    <li key={movie.id}>
+                        {movie.title}
+                    </li>
+                ))}
+            </ul>
         </div>
     );
-};
 
+}
 export default DropDown
